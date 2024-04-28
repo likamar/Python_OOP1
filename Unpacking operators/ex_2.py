ab68aef1b50b69701670a3220684eaa2eb7d6419
likamar <lika.marcin@gmail.com>
2024-04-28 14:55:44 +0200

    refactor Unpacking operators ex_2

--- Unpacking operators/ex_2.py
+++ Unpacking operators/ex_2.py
@@ -3,9 +3,12 @@
 # znak równa się i wartość (np. first_name=Mikołaj, age=134).
 
 
-def print_self(**kwargs):
+def print_call_str(**kwargs):
+    call_str= "print_call_str("
     for key, value in kwargs.items():
-        print(key, "=", value, sep="")
+        call_str += f"{key}={value},"
+    call_str = call_str[:-1] + ")"
+    print(call_str)
 
 
-print_self(first_name="Marcin", surname="Lika", age=35, email="marcin@lika.com")
+print_call_str(first_name="Marcin", surname="Lika", age=35, email="marcin@lika.com")
