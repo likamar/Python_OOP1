aa7d4cb7121cfb53b25ac26ae15c003caae13d1f
likamar <lika.marcin@gmail.com>
2024-05-04 20:32:33 +0200

    DiscountPolicy - change implementation

--- Constructor, object fields and methods/shop/discount_policy.py
+++ Constructor, object fields and methods/shop/discount_policy.py
@@ -1,18 +1,4 @@
 class DiscountPolicy:
 
-    LOYAL_CUSTOMER_DISCOUNT = 0.05
-    CHRISTMAS_DISCOUNT = 20
-
-    @classmethod
-    def default(cls, total_price):
-        return total_price
-
-    @classmethod
-    def loyal_customer(cls, total_price):
-        return total_price * (1 - cls.LOYAL_CUSTOMER_DISCOUNT)
-
-    @classmethod
-    def christmas(cls, total_price):
-        if total_price > 100:
-            return total_price - cls.CHRISTMAS_DISCOUNT
+    def apply_discount(self, total_price):
         return total_price
